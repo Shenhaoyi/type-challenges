d1430fe472379293ab0373345213878fb6ba5747
shenhaoyi <shenhaoyi04@163.com>
2022-02-09 12:05:27 +0800

    完成9

---
+++ answers/9-medium-deep-readonly/template.ts
@@ -0,0 +1,13 @@
+type DeepReadonly<T extends object> = {
+  +readonly [key in keyof T]: T[key] extends object
+  ? T[key] extends () => any // Function
+    ? T[key]
+    : DeepReadonly<T[key]>
+  : T[key]
+}
+// 判断是否有属性的方式 keyof T[key] extends never
+// type DeepReadonly2<T> = {
+//   +readonly [key in keyof T]: keyof T[key] extends never
+//   ? T[key]
+//   : DeepReadonly<T[key]> // 这里的类型检测不通过，判断不了是否为object
+// }
---
+++ answers/9-medium-deep-readonly/test-cases.ts
@@ -0,0 +1,40 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<DeepReadonly<X>, Expected>>,
+]
+
+type X = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+    }
+  }
+}
+type test2232 = DeepReadonly<X>;
+
+type Expected = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+    }
+  }
+}
