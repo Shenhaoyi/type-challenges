dbf032fa515da8c682209e1713f1ed29a4ea61e6
shenhaoyi <shenhaoyi04@163.com>
2022-02-16 15:24:35 +0800

    模板字面量类型推断

--- my-test/template-literal-types.ts
+++ my-test/template-literal-types.ts
@@ -9,5 +9,13 @@ type HelloWorld1 = `Hello ${World}`
 type TestWorld = "test" | "world"
 type TestWorld2 = `${TestWorld}_id`
 
+// infer & extends 将模板字面量类型与条件类型结合进行类型推断
+// https://davidtimms.github.io/programming-languages/typescript/2020/11/20/exploring-template-literal-types-in-typescript-4.1.html
+// 匹配字符串中的部分内容，匹配的边缘需要有明确的特征
+type ExtractVerb<S extends string> = S extends `${infer Verb} ${infer Activity}` ? Verb : never;
+type TestExtractVerb = ExtractVerb<'shen haoyi'>
+// 字符串如果没有明确的特征, 会直接匹配到第一个字符？（测试结果，未找到出处）
+type FirstLetter<S extends string> = S extends `${infer First}${infer Rest}` ? First : never;
+type TestFirstLetter = FirstLetter<'shenhaoyi'>
 
 // 文档没有看完，只看了前言部分
